665bb6f855c6b824c0b03b314517be5b61eb3105
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-10-26 08:30:24 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex min-h-screen items-center justify-center bg-slate-800'>
+      <h1 className='text-3xl font-bold text-teal-500'>Vite + React!</h1>
+    </div>
+  )
+}
+
+export default App
