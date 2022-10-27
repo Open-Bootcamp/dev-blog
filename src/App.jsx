b6eadaae4a316f39ed6fb83072f59b47d1777591
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2022-10-27 09:31:47 +0200

    modified route tag to tags

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route exact path={'/'} element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/tag' element={<Tag />} />
+        <Route path='/tags' element={<Tag />} />
         // TODO: page 404
       </Routes>
     </Router>
