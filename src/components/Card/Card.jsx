60cd287cc7b755e202c8d8dfebefe1995ef1b777
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-10-27 18:47:30 -0500

    feat: card component desktop

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,33 @@
 import propTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 function Card({ article }) {
   return (
-    <article className='flex flex-col overflow-hidden rounded-md bg-white shadow-sm'>
-      <img src={article.image} alt='article image' className='w-full object-cover' />
-      <div className=' flex flex-col p-6'>
-        <span className='text-xs uppercase text-gray-500 font-medium'>{article.tags[0]}</span>
-        <h2 className='text-lg font-bold leading-6'>{article.title}</h2>
-        <p className='mt-2'>{article.preview}</p>
-        <div className='flex justify-between items-center mt-6'>
-          <img src={article.authorImage} alt='author image' className='w-8 h-8 object-cover rounded-full' />
-          <span className='uppercase text-xs text-gray-500 font-medium'>{article.timeRead} min read</span>
+    <article className='flex flex-1 flex-col overflow-hidden rounded-md bg-white shadow-sm transition-transform hover:scale-105 md:basis-60 md:first:basis-full md:first:flex-row'>
+      <Link to={`/article/${article.id}`} className='flex flex-[2]'>
+        <img
+          src={article.image}
+          alt='article image'
+          className='w-full object-cover'
+        />
+      </Link>
+      <div className='flex flex-1 flex-col p-6'>
+        <Link  to={`/article/${article.id}`}>
+          <span className='text-xs font-medium uppercase text-gray-500'>
+            {article.tags[0]}
+          </span>
+          <h2 className='text-lg font-bold leading-6'>{article.title}</h2>
+          <p className='mt-2'>{article.preview}</p>
+        </Link>
+        <div className='mt-6 flex flex-1 items-end justify-between'>
+          <img
+            src={article.authorImage}
+            alt='author image'
+            className='h-8 w-8 rounded-full object-cover'
+          />
+          <span className='text-xs font-medium uppercase text-gray-500'>
+            {article.timeRead} min read
+          </span>
         </div>
       </div>
     </article>
--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,5 @@
+import logoOb from '../assets/ob.png'
+
 export default [
   {
     id: 1,
@@ -6,7 +8,7 @@ export default [
     tags: ['react', 'tailwind', 'ui'],
     image:
       'https://cdn.pixabay.com/photo/2015/12/04/14/05/code-1076536_960_720.jpg',
-    authorImage: 'https://cdn.pixabay.com/photo/2016/11/18/19/07/happy-1836445_960_720.jpg',
+    authorImage: logoOb,
     content: `
     En este artículo vamos a crear un blog simple utilizando como tecnologías a React y Tailwind.
 
@@ -32,6 +34,7 @@ export default [
     tags: ['react', 'tailwind', 'ui'],
     image:
       'https://cdn.pixabay.com/photo/2015/12/04/14/05/code-1076536_960_720.jpg',
+    authorImage: logoOb,
     content: `
     En este artículo vamos a crear un blog simple utilizando como tecnologías a React y Tailwind.
 
@@ -57,6 +60,7 @@ export default [
     tags: ['react', 'tailwind', 'ui'],
     image:
       'https://cdn.pixabay.com/photo/2015/12/04/14/05/code-1076536_960_720.jpg',
+    authorImage: logoOb,
     content: `
     En este artículo vamos a crear un blog simple utilizando como tecnologías a React y Tailwind.
 
@@ -82,6 +86,59 @@ export default [
     tags: ['react', 'tailwind', 'ui'],
     image:
       'https://cdn.pixabay.com/photo/2015/12/04/14/05/code-1076536_960_720.jpg',
+    authorImage: logoOb,
+    content: `
+    En este artículo vamos a crear un blog simple utilizando como tecnologías a React y Tailwind.
+
+    - [ ] React
+    - [ ] Tailwind
+
+    ## Contenido
+
+    Esta es la prueba de contenido
+
+    ### Descripción
+
+    Esto es una descripción
+    `,
+    preview:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id ornare lectus. Duis interdum lectus non ipsum pulvinar, sit amet luctus massa semper...',
+    timeRead: 4,
+  },
+  {
+    id: 5,
+    title: 'Creando un blog con React + Tailwind desde cero',
+    date: new Date(),
+    tags: ['react', 'tailwind', 'ui'],
+    image:
+      'https://cdn.pixabay.com/photo/2015/12/04/14/05/code-1076536_960_720.jpg',
+    authorImage: logoOb,
+    content: `
+    En este artículo vamos a crear un blog simple utilizando como tecnologías a React y Tailwind.
+
+    - [ ] React
+    - [ ] Tailwind
+
+    ## Contenido
+
+    Esta es la prueba de contenido
+
+    ### Descripción
+
+    Esto es una descripción
+    `,
+    preview:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id ornare lectus. Duis interdum lectus non ipsum pulvinar, sit amet luctus massa semper...',
+    timeRead: 4,
+  },
+  {
+    id: 6,
+    title: 'Creando un blog con React + Tailwind desde cero',
+    date: new Date(),
+    tags: ['react', 'tailwind', 'ui'],
+    image:
+      'https://cdn.pixabay.com/photo/2015/12/04/14/05/code-1076536_960_720.jpg',
+    authorImage: logoOb,
     content: `
     En este artículo vamos a crear un blog simple utilizando como tecnologías a React y Tailwind.
 
