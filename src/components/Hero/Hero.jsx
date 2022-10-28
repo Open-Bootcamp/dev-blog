988a85ccc560bc11bcd381fe6012e842e7544aa1
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2022-10-29 01:09:17 +0200

    fix:hero-component-> add background-image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,10 +4,8 @@ import PropTypes from 'prop-types';
 
 const Hero = ({author}) => {
 
-  const bgImgHero= "[url(../../assets/developers.jpeg)]"
-
   return (
-    <header className='container  sm:h-96 my-6 bg-gradient-to-r  from-violet-500 to-pink-500'>
+    <header className='container  sm:h-96 my-6 bg-cover bg-hero-img' >
       <div className='flex flex-col items-center p-16'>
         <div className=''>
           <img className='h-24 w-24 md:h-32 md:w-32 rounded-full object-cover  border-double border-8 shadow-2xl shadows-white' src='../../../src/assets/ob.png' alt=''></img>
