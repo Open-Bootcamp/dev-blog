dc2de74301c5d2edd51e19d45e5ca8870e2329a9
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2022-11-01 18:13:25 +0100

    fix margin-Header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,7 +13,7 @@ const HeroCopy = ({author, isImage}) => {
        ((isImage !== false
         ?
        (
-        <header className='container-custom  sm:h-96 my-6 bg-cover bg-hero-img' >
+        <header className='container-custom my-0 sm:h-96  bg-cover bg-hero-img' >
         <div className='flex flex-col items-center p-16'>
 
           <img className='h-24 w-24 md:h-32 md:w-32 rounded-full object-cover  border-double border-8 shadow-2xl shadows-white' src='../../../src/assets/ob.png' alt=''></img>
@@ -24,19 +24,16 @@ const HeroCopy = ({author, isImage}) => {
        )
        :
         (
-          <header className='container-custom  sm:h-96 my-6 bg-black' >
+          <header className='container-custom  sm:h-96 bg-black' >
             <div className='flex flex-col items-center p-16'>
               <img className='h-24 w-24 md:h-32 md:w-32 rounded-full object-cover  border-double border-8 shadow-2xl shadows-white' src='../../../src/assets/ob.png' alt=''></img>
               <div className='text-white text-2xl md:text-4xl font-bold m-1 text-center'>{`${tagParam.tag}`}</div>
-
               <p className=' text-lg text-white font-light text-center'>Hay una coleccion de {articleTag.length} articulos</p>
             </div>
           </header>
         )
 
         )  )
-
-
   );
 };
 
